bb0c1c411266b5f964064415494d578ecdde87db
Levi-24 <leventemajor36@gmail.com>
2024-02-05 11:11:00 +0100

    Add project files.

---
+++ Emelet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkoloHaz
+{
+    internal class Emelet
+    {
+        public int sorszam { get; set; }
+        public string emeletNev { get; set; }
+        public List<int> szektorAdatok { get; set; }
+
+        public Emelet(string r, int sorszam)
+        {
+            this.sorszam = sorszam;
+            var adatok = r.Split(';');
+            this.emeletNev = adatok[0].PadRight(10);
+            this.szektorAdatok = new List<int>();
+            for (int i = 1; i < adatok.Length; i++)
+            {
+                szektorAdatok.Add(int.Parse(adatok[i]));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{sorszam}. {emeletNev} \t {string.Join("\t\t", szektorAdatok)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ParkoloHaz;
+
+List<Emelet> parkoloHaz = new List<Emelet>();
+int sorszam = 1;
+using (var sr = new StreamReader("../../../src/parkolohaz.txt"))
+{
+    while (!sr.EndOfStream)
+    {
+        parkoloHaz.Add(new Emelet(sr.ReadLine(), sorszam));
+        sorszam++;
+    }
+}
+
+Console.WriteLine("Szint neve\t Szektor1\tSzektor2\tSzektor3\tSzektor4\tSzektor5\tSzektor6");
+foreach (var emelet in parkoloHaz) Console.WriteLine(emelet);
